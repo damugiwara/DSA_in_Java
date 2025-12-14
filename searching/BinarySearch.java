90ed0557beacb96b20de34c7511763e8a43cc12c
Vansh Tripathi <1vansh.tripathi0@gmail.com>
2025-12-14 21:34:19 +0530

    Improve BinarySearch class by switching to non-static Search object method

--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -1,5 +1,5 @@
 class Search {
-    public static int binarySearch(int[] arr, int low, int high, int target){
+    public int binarySearch(int[] arr, int low, int high, int target){
 
         if(low > high){
             return -1;
@@ -28,7 +28,9 @@ public class BinarySearch{
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int target = 9;
 
-        int result = Search.binarySearch(arr, 0, arr.length - 1, target);
+        Search s = new Search();
+        int result = s.binarySearch(arr, 0, arr.length - 1, target);
+
         System.out.printf("Element found at index: %d", result);
 
 
